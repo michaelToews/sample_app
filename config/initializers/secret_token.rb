97baddb1344ca588ad86b9e7ad03ce6fc8a4ad26
Michael Toews <michael@rhomobile.com>
2012-03-21 17:52:17 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '39fedf030f8da663b96cde9a1a61fed7dd45b048c12ab0074a874b4c734ce5f863bf6bf5bf98d2aa2f571bac82903485f72194ac30c6607190ec69cdc708f41b'
